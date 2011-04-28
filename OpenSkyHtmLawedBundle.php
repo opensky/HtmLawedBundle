282bc1ace4e3374214cc32cc42d4fd89b208de00
Kris Wallsmith <kris.wallsmith@gmail.com>
2011-04-28 10:11:25 -0700

    fixed extension alias

--- OpenSkyHtmLawedBundle.php
+++ OpenSkyHtmLawedBundle.php
@@ -3,16 +3,12 @@
 namespace OpenSky\Bundle\HtmLawedBundle;
 
 use OpenSky\Bundle\HtmLawedBundle\DependencyInjection\OpenSkyHtmLawedExtension;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class OpenSkyHtmLawedBundle extends Bundle
 {
-    /**
-     * @see Symfony\Component\HttpKernel\Bundle.Bundle::build()
-     */
-    public function build(ContainerBuilder $container)
+    public function __construct()
     {
-        $container->registerExtension(new OpenSkyHtmLawedExtension());
+        $this->extension = new OpenSkyHtmLawedExtension();
     }
 }
